754e0f2d0cc50f295a0472b16d6d59383b6d4787
laptop <cspruce89@gmail.com>
2016-04-02 23:10:12 -0500

    Item Review and Item Generation
    Why wont this work?

---
+++ Prototype/Assets/Item_Generation.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+import System.Collections.Generic;
+
+
+
+
+var itemsList :  List.<Item_Review> = new List.<Item_Review>();
+
+var numberToGenerate : int;
+
+
+function Start () {
+    
+}
+
+function Update () {
+    Generate();
+}
+
+function Generate()
+{
+    if(itemsList.Count < numberToGenerate)
+    {
+        var x : Item_Review = new Item_Review();
+        itemsList.Add(x);
+    }
+}
---
+++ Prototype/Assets/Item_Review.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+
+public class Item_Review
+{
+
+    var nam : String;
+
+    var id : float;
+
+    var rating : int;
+
+    var date : int;
+
+    function Item_Review()
+    {
+        nam = "Customer Name";
+        id = 0;
+        rating = Random.Range(0,5);
+        date = Random.Range(0,365);
+        
+    }
+
+    }
